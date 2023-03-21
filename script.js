b46cadcdcf3c1b544a45a296cd2232f426b63e6a
YUSUF HASSAN <gameaddictionz123@gmail.com>
2023-03-21 16:57:45 +0000

    local storage functions for past searched cities

--- script.js
+++ script.js
@@ -32,4 +32,55 @@ $(document).ready(function () {
        }
        return comparison;
    }
+   function loadCities() {
+    const storedCities = JSON.parse(localStorage.getItem('pastCities'));
+    if (storedCities) {
+        pastCities = storedCities;
+    }
+}
+
+
+function storeCities() {
+    localStorage.setItem('pastCities', JSON.stringify(pastCities));
+}
+
+
+
+function buildURLFromInputs(city) {
+    if (city) {
+        return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    }
+}
+
+function buildURLFromId(id) {
+    return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
+}
+
+ 
+ function displayCities(pastCities) {
+    cityListEl.empty();
+    pastCities.splice(5);
+    let sortedCities = [...pastCities];
+    sortedCities.sort(compare);
+    sortedCities.forEach(function (location) {
+        let cityDiv = $('<div>').addClass('col-12 city');
+        let cityBtn = $('<button>').addClass('btn btn-light city-btn').text(location.city);
+        cityDiv.append(cityBtn);
+        cityListEl.append(cityDiv);
+    });
+}
+
+
+function setUVIndexColor(uvi) {
+    if (uvi < 3) {
+        return 'green';
+    } else if (uvi >= 3 && uvi < 6) {
+        return 'yellow';
+    } else if (uvi >= 6 && uvi < 8) {
+        return 'orange';
+    } else if (uvi >= 8 && uvi < 11) {
+        return 'red';
+    } else return 'purple';
+}
+
 });
